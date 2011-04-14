33746377845b6bb603767a206ff929713c51e4d7
Ondřej Bartas <o.bartas@gmail.com>
2011-04-14 19:54:04 +0200

    opraven initializer for fallbacks

--- lib/config/initializers/has_translation.rb
+++ lib/config/initializers/has_translation.rb
@@ -1,3 +1,5 @@
+#ALLOWING FALLBACK !!!!
+require "i18n/backend/fallbacks" 
 require 'app/models/i18n_backend_availible_locales'
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 I18n.fallbacks[:cz] => [:cz, :en, :es]
--- lib/has_translation.rb
+++ lib/has_translation.rb
@@ -4,6 +4,3 @@ require 'app/models/has_translation'
 require 'app/models/has_translation/translation'
 require 'app/helpers/set_locale_helper'
 include SetLocaleHelper
-
-#ALLOWING FALLBACK !!!!
-require "i18n/backend/fallbacks" 
